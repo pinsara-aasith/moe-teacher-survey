8116592523047880d33e8612f0d560957c3ae783
Pinsara Aasith <aasithp@gmail.com>
2024-07-06 18:07:06 +0530

    disable manually creating database schema in db.js

--- data-analytics/db.js
+++ data-analytics/db.js
@@ -3,79 +3,12 @@ import { MongoClient, ObjectId } from 'mongodb';
 const dbName = process.env.DATABASE_NAME;
 let mongoClient = null;
 
-const DB_SCHEMA = {
-    collections: [
-        {
-            name: 'surveyRecords',
-            indexes: [
-                {
-                    indexSpec: { nic: 1 },
-                    options: { unique: true, name: 'nic' }
-                }
-            ]
-        },
-        {
-            name: 'teachers',
-            indexes: [
-                {
-                    indexSpec: { nic: 1 },
-                    options: { unique: true, name: 'nic' }
-                }
-            ]
-        },
-        {
-            name: 'school_admins',
-            indexes: [
-                {
-                    indexSpec: { id: 1 },
-                }
-            ]
-        },
-        {
-            name: 'schools',
-            indexes: [
-                { indexSpec: { zone: 1 } },
-                { indexSpec: { division: 1 } },
-                { indexSpec: { province: 1 } },
-            ]
-        }
-    ]
-}
-
-const generateSchema = async (client) => {
-    try {
-        const db = client.db(dbName);
-
-        for (const collectionSchema of DB_SCHEMA.collections) {
-            const collectionName = collectionSchema.name;
-            const existingCollections = await db.listCollections({ name: collectionName }).toArray();
-
-            if (existingCollections.length !== 0) {
-                continue;
-            }
-
-            await db.createCollection(collectionName);
-            console.log(`Collection ${collectionName} created`);
-
-            const collection = db.collection(collectionName);
-            for (const index of collectionSchema.indexes) {
-                await collection.createIndex(index.indexSpec, index.options);
-                console.log(`Index ${index.options.name} created/synced for collection ${collectionName}.`);
-            }
-        }
-    } catch (error) {
-        console.error('Error creating database schema or indexes:', error);
-    }
-}
-
 const getClient = async () => {
     try {
         if (mongoClient) return mongoClient;
 
         mongoClient = new MongoClient(process.env.MONGODB_URI, { minPoolSize: 1, maxPoolSize: 20 });
         await mongoClient.connect();
-        await generateSchema(mongoClient);
-
         return mongoClient;
 
     } catch (error) {
@@ -123,6 +56,5 @@ export {
     insertOne,
     updateOne,
     deleteOne,
-    findById,
-    generateSchema
+    findById
 }
